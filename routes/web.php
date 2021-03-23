3f3dd350a920d5babfd51dc5e9792d1d49e26f1f
pc-work <adipol13@gmail.com>
2021-03-23 14:39:10 -0400

    laravel import export excel csv

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExcelController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route for view/blade file.
+Route::get('importExportView', [ExcelController::class, 'importExportView'])->name('importExportView');
+// Route for export/download tabledata to .csv, .xls or .xlsx
+Route::get('exportExcel/{type}', [ExcelController::class, 'exportExcel'])->name('exportExcel');
+// Route for import excel data to database.
+Route::post('importExcel', [ExcelController::class, 'importExcel'])->name('importExcel');
